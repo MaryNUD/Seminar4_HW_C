f0e69c35e2fcebf74231181e848e2a407882a0e3
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-12 20:01:55 +0300

    add task 29

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[n];
+void fill(int[] collection);
+{
+    int length = collection.length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 101);
+        index++;
+    }
+}
+
+// Не понимаю почему, то что делали на лекциях не работает... Вроде как пишу то же,что и было показано.
