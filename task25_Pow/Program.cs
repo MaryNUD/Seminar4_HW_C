41908e50dd5c48add7e5f3c6b8f13c587f2d734c
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-12 19:52:54 +0300

    add task 25

---
+++ task25_Pow/Program.cs
@@ -0,0 +1,11 @@
+﻿int f(int num, int s);
+{
+   int n = Math.Pow(num, s);
+   return n;
+}
+
+Console.Write("Insert the numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a, b));
+
