f96cc583a020c1e84fbcaa5168b7b0552086532b
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-12 19:53:19 +0300

    add task 27

---
+++ task27/Program.cs
@@ -0,0 +1,12 @@
+﻿void f (int x);
+{
+    int i = 0;
+    while (x > 0) 
+    {
+        i = i + x % 10;
+        x = x / 10;
+    }
+}
+Console.Write("Insert the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(f(n));
